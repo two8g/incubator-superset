447bd65c4c4fc8b38e129b32cfca0ede3a72d992
binxin <binxin.yi@colorv.com>
2018-12-29 15:07:38 +0800

    fix date time trunc

--- superset/db_engine_specs.py
+++ superset/db_engine_specs.py
@@ -670,10 +670,10 @@ class PrestoEngineSpec(BaseEngineSpec):
 
     time_grain_functions = {
         None: '{col}',
-        'PT1S': "date_trunc('second', CAST({col} AS TIMESTAMP))",
-        'PT1M': "date_trunc('minute', CAST({col} AS TIMESTAMP))",
-        'PT1H': "date_trunc('hour', CAST({col} AS TIMESTAMP))",
-        'P1D': "date_trunc('day', CAST({col} AS TIMESTAMP))",
+        'PT1S': "CAST({col} AS TIMESTAMP)",
+        'PT1M': "sub_str(CAST({col} AS TIMESTAMP),0,15)",
+        'PT1H': "sub_str(CAST({col} AS TIMESTAMP),0,13)",
+        'P1D': "to_date(CAST({col} AS TIMESTAMP))",
         'P1W': "date_trunc('week', CAST({col} AS TIMESTAMP))",
         'P1M': "date_trunc('month', CAST({col} AS TIMESTAMP))",
         'P0.25Y': "date_trunc('quarter', CAST({col} AS TIMESTAMP))",
